523d3fd8d8026a6331320c36372be71e0bb60f6e
Daniel Aguirre <main.data06@gmail.com>
2024-04-04 19:35:34 -0300

    Primer componente Navbar

---
+++ tutienda/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <header>
+            <h1>TuTiendaPokemon</h1>
+            <nav>
+                <a href="">Agua</a>
+                <a href="">Fuego</a>
+                <a href="">Planta</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
