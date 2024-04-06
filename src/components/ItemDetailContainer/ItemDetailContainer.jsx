25b1e5f300018b762b072f883da43475ba51e70a
Daniel Aguirre <main.data06@gmail.com>
2024-04-06 18:53:13 -0300

    Se arregla el repositorio para que funcione vercel

---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,26 @@
+import { useState , useEffect } from "react"
+import {getDoc,doc} from 'firebase/firestore'
+import ItemDetail from "../ItemDetail/ItemDetail"
+import { useParams} from "react-router-dom"
+import { db } from "../../../services/firebase/firebaseConfig"
+const ItemDetailContainer = () =>{
+    const [product, setProduct] = useState(null)
+
+    const { itemId } = useParams()
+    useEffect(()=> {
+        const productDoc = doc(db,'products', itemId)
+        getDoc(productDoc)
+            .then(queryDocumentSnapshot=>{
+                const data=queryDocumentSnapshot.data()
+                const productAdapted ={id: queryDocumentSnapshot.id,...data}
+                setProduct(productAdapted)
+            })
+    },  [itemId])
+    return (
+        <main>
+            <h1>Detalle de productos</h1>
+            <ItemDetail {...product}/>
+        </main>
+    )
+}
+export default ItemDetailContainer
